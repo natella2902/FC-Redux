1d9dd4aa1d34f38bb2c654f533453cd6ae6d2dde
natella2902 <n.jelonkina@mail.ru>
2022-01-23 20:26:12 +0300

    edit: professions store

---
+++ src/app/App.js
@@ -0,0 +1,48 @@
+import React, { useEffect } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Users from "./layouts/users";
+import Login from "./layouts/login";
+import Main from "./layouts/main";
+import NavBar from "./components/ui/navBar";
+import { ToastContainer } from "react-toastify";
+import { ProfessionProvider } from "./hooks/useProfession";
+import AuthProvider from "./hooks/useAuth";
+import ProtectedRoute from "./components/common/protectedRoute";
+import LogOut from "./layouts/logOut";
+import { useDispatch } from "react-redux";
+import { loadQualitiesList } from "./store/qualities";
+import { loadProfessionsList } from "./store/professions";
+
+function App() {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(loadQualitiesList());
+    }, []);
+    useEffect(() => {
+        dispatch(loadProfessionsList());
+    }, []);
+    return (
+        <div>
+            <AuthProvider>
+                <NavBar />
+
+                <ProfessionProvider>
+                    <Switch>
+                        <ProtectedRoute
+                            path="/users/:userId?/:edit?"
+                            component={Users}
+                        />
+                        <Route path="/login/:type?" component={Login} />
+                        <Route path="/logout" component={LogOut} />
+                        <Route path="/" exact component={Main} />
+                        <Redirect to="/" />
+                    </Switch>
+                </ProfessionProvider>
+            </AuthProvider>
+
+            <ToastContainer />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/app/store/createStore.js
@@ -0,0 +1,15 @@
+import qualitiesReducer from "./qualities";
+import professionsReducer from "./professions";
+
+const { combineReducers, configureStore } = require("@reduxjs/toolkit");
+
+const rootReducer = combineReducers({
+    qualities: qualitiesReducer,
+    professions: professionsReducer
+});
+
+export function createStore() {
+    return configureStore({
+        reducer: rootReducer
+    });
+}
